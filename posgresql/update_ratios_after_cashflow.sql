872575ca2d88cb327f429826a3fb8c975185777a
milosfxc <milosfxc@gmail.com>
2024-07-16 11:34:04 +0200

    Added trigger functions for the database.

--- posgresql/update_ratios_after_cashflow.sql
+++ posgresql/update_ratios_after_cashflow.sql
@@ -4,22 +4,23 @@ DECLARE
     _price d_timeframe.close%type;
     _pcf ratios.pcf%type;
     _pfcf ratios.free_cash_flow%type;
+    _dividend_yield ratios.dividend_yield%type;
+    _dividend_payout_ratio ratios.dividend_payout_ratio%type;
     --calculated
     _wei_shs_out NUMERIC;
     _shs_out NUMERIC;
     _ocfps NUMERIC;
     _market_cap NUMERIC;
-
-
+    _net_income NUMERIC;
 BEGIN
 --Price
 SELECT close INTO _price FROM d_timeframe WHERE share_id = NEW.share_id AND date <= NEW.date ORDER BY date DESC LIMIT 1;
 
 --WEIGHTED SHARES OUTSTANDING
-    SELECT weighted_avg_shares_outstanding INTO _wei_shs_out FROM income_statement WHERE share_id = NEW.share_id AND date = NEW.date;
+SELECT weighted_avg_shares_outstanding INTO _wei_shs_out FROM income_statement WHERE share_id = NEW.share_id AND date = NEW.date;
 
 --SHARES OUTSTANDING
-    SELECT common_shares_outstanding INTO _shs_out FROM trade_info WHERE share_id = NEW.share_id AND date = NEW.date;
+SELECT common_shares_outstanding INTO _shs_out FROM trade_info WHERE share_id = NEW.share_id AND date = NEW.date;
 
 --OPERATING CASH FLOW PER SHARE
 IF NEW.operating_cash_flow IS NOT NULL AND _wei_shs_out IS NOT NULL AND _wei_shs_out <> 0 THEN
@@ -36,25 +37,42 @@ ELSE
 END IF;
 
 --MARKET CAP
-IF _shs_out IS NOT NULL AND _shs_out <> 0 AND _price IS NOT NULL AND _price <> 0 THEN
+IF _shs_out IS NOT NULL AND _shs_out > 0 AND _price IS NOT NULL AND _price > 0 THEN
     _market_cap := _shs_out * _price;
 ELSE
     _market_cap := NULL;
 END IF;
---PFCF
+--PRICE TO FREE CASH FLOW
 IF _market_cap IS NOT NULL AND NEW.free_cash_flow IS NOT NULL THEN
     _pfcf := ROUND(_market_cap / NEW.free_cash_flow, 2);
 ELSE
     _pfcf := NULL;
 END IF;
 
+--DIVIDEND YIELD
+IF NEW.dividends_paid IS NOT NULL AND NEW.dividends_paid > 0 AND  _market_cap IS NOT NULL THEN
+    _dividend_yield := ROUND((NEW.dividends_paid / _market_cap) * 100, 2);
+ELSE
+    _dividend_yield := 0;
+END IF;
+
+--DIVIDEND PAYOUT RATIO
+IF NEW.dividends_paid IS NOT NULL AND NEW.dividends_paid > 0 AND _net_income IS NOT NULL AND _net_income <> 0 THEN
+    _dividend_payout_ratio := ROUND(NEW.dividends_paid / _net_income, 2);
+ELSE
+    _dividend_payout_ratio := NULL;
+END IF;
+
+
 -- Upsert
-INSERT INTO ratios (share_id, date, pcf, pfcf)
-VALUES (NEW.share_id, NEW.date, _pcf, _pfcf)
+INSERT INTO ratios (share_id, date, pcf, pfcf, dividend_yield, dividend_payout_ratio)
+VALUES (NEW.share_id, NEW.date, _pcf, _pfcf, _dividend_yield, _dividend_payout_ratio)
 ON CONFLICT (share_id, date) DO UPDATE SET
     pe = EXCLUDED.pe,
     pcf = EXCLUDED.pcf,
-    pcfc = EXCLUDED.pfcf;
+    pcfc = EXCLUDED.pfcf,
+    dividend_yield = EXCLUDED.dividend_yield,
+    dividend_payout_ratio = EXCLUDED.dividend_payout_ratio;
 
 RETURN NEW;
 END;
--- posgresql/update_ratios_after_income_statement.sql
+++ posgresql/update_ratios_after_income_statement.sql
@@ -5,8 +5,16 @@ DECLARE
     _pe ratios.pe%type;
     _eps_yoy ratios.eps_yoy%type;
     _ps ratios.ps%type;
+    _roa ratios.roa%type;
+    _roe ratios.roa%type;
+    _gross_margin ratios.gross_margin%type;
+    _operating_margin ratios.operating_margin%type;
+    _ebitda_margin ratios.ebitda_margin%type;
+    _net_profit_margin ratios.net_profit_margin%type;
     --calculated
     _previous_eps NUMERIC;
+    _assets NUMERIC;
+    _equity NUMERIC;
 BEGIN
 --Price
 SELECT close INTO _price FROM d_timeframe WHERE share_id = NEW.share_id AND date <= NEW.date ORDER BY date DESC LIMIT 1;
@@ -32,13 +40,69 @@ IF NEW.weighted_avg_shares_outstanding IS NOT NULL AND NEW.weighted_avg_shares_o
 ELSE
     _ps := NULL;
 END IF;
--- Upsert
-INSERT INTO ratios (share_id, date, pe, eps_yoy, ps)
-VALUES (NEW.share_id, NEW.date, _pe, _eps_yoy, _ps)
+
+--ASSETS
+SELECT assets INTO _assets FROM balance_sheet WHERE share_id = NEW.share_id AND date = NEW.date;
+
+--ROA
+IF NEW.net_income IS NOT NULL AND _assets IS NOT NULL AND _assets <> 0 THEN
+    _roa := ROUND((NEW.net_income / _assets) * 100, 2);
+ELSE
+    _roa := NULL;
+END IF;
+
+--EQUITY
+SELECT equity INTO _equity FROM balance_sheet WHERE share_id = NEW.share_id AND date = NEW.date;
+
+--ROE
+IF NEW.net_income IS NOT NULL AND _equity IS NOT NULL AND _equity <> 0 THEN
+    _roe := ROUND((NEW.net_income / _equity) * 100, 2);
+ELSE
+    _roe := NULL;
+END IF;
+
+--GROSS MARGIN
+IF NEW.gross_profit IS NOT NULL AND NEW.revenue IS NOT NULL AND NEW.revenue <> 0 THEN
+    _gross_margin := ROUND(NEW.gross_profit / NEW.revenue, 2);
+ELSE
+    _gross_margin := NULL;
+END IF;
+
+--OPERATING MARGIN
+IF NEW.ebit IS NOT NULL AND NEW.revenue IS NOT NULL AND NEW.revenue <> 0 THEN
+    _operating_margin := ROUND(NEW.ebit / NEW.revenue, 2);
+ELSE
+    _operating_margin := NULL;
+END IF;
+
+--EBITDA MARGIN
+IF NEW.ebitda IS NOT NULL AND NEW.revenue IS NOT NULL AND NEW.revenue <> 0 THEN
+    _ebitda_margin := ROUND(NEW.ebitda / NEW.revenue, 2);
+ELSE
+    _ebitda_margin := NULL;
+END IF;
+
+--NET PROFIT MARGIN
+IF NEW.net_income IS NOT NULL AND NEW.revenue IS NOT NULL AND NEW.revenue <> 0 THEN
+    _net_profit_margin := ROUND(NEW.net_income / NEW.revenue, 2);
+ELSE
+    _net_profit_margin := NULL;
+END IF;
+
+
+-- UPSERT
+INSERT INTO ratios (share_id, date, pe, eps_yoy, ps, roa, roe, gross_margin, operating_margin, ebitda_margin, net_profit_margin)
+VALUES (NEW.share_id, NEW.date, _pe, _eps_yoy, _ps, _roa, _roe, _gross_margin, _operating_margin, _ebitda_margin, _net_profit_margin)
 ON CONFLICT (share_id, date) DO UPDATE SET
     pe = EXCLUDED.pe,
     eps_yoy = EXCLUDED.eps_yoy,
-    ps = EXCLUDED.ps;
+    ps = EXCLUDED.ps,
+    roa = EXCLUDED.roa,
+    roe = EXCLUDED.roe,
+    gross_margin = EXCLUDED.gross_margin,
+    operating_margin = EXCLUDED.operating_margin,
+    ebitda_margin = EXCLUDED.ebitda_margin,
+    net_profit_margin = EXCLUDED.net_profit_margin;
 
 RETURN NEW;
 END;
