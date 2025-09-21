3f22507c59354d0e2cb5c5744fdccbedb4abaa20
milosfxc <milosfxc@gmail.com>
2025-09-21 16:41:18 +0200

    Adding for any case

--- test.py
+++ test.py
@@ -43,34 +43,9 @@ def add(number: int) -> int:
 
 if __name__ == "__main__":
     x1 = time.perf_counter()
-    # filings = edgar.get_filings(filing_date='2025-09-02', form=['10-Q'])
-    # download_filings(filing_date='2025-09-02', filings=filings,compress=False)
+    fundamentals_service.get_company_fundamentals('WEYS',60,config.report_start_date)
 
-    # filings = edgar.get_filings(filing_date='2025-09-02', form=['10-Q']).to_pandas()
-    # print(filings)
-    edgar.download_edgar_data()
-    online_time = time.perf_counter()
-    bs = edgar.get_by_accession_number(accession_number='0001166388-25-000117')
-    print(time.perf_counter() - online_time)
-    print(bs)
-    # edgar.use_local_storage(True)
-    # offline_time = time.perf_counter()
-    # bs = edgar.get_by_accession_number(accession_number='0001817358-25-000158').xbrl().statements.balance_sheet().to_dataframe()
-    # print(time.perf_counter() - offline_time)
-    # print(bs)
-    # download_filings(filing_date='2025-01-01:2025-09-19', filings=filings)
-    # yahoo_service.get_company_fundamentals(ticker='AEP', share_id=71, nearby_report_date=pd.Timestamp(year=2025, month=3, day=31))
 
-    # df = edgar.get_by_accession_number('0000004904-25-000027').xbrl().statements.income_statement().to_dataframe()
-    # df_dict = get_by_accession_number('0000012208-25-000052')
-    # df = get_by_accession_number('0001213900-25-024561').obj().financials.income_statement().to_dataframe().replace(['',np.nan],None)
-    # df = get_by_accession_number('0001045810-25-000209').xbrl().query().by_concept('us-gaap:Revenue').by_instant_date('2025-07-27').to_dataframe()]
-    # df_dict = df.to_dict()
-    # with open('output.json', "w") as f:
-    #     json.loads(df_dict, f, indent=4)  # indent for pretty printing
-    #
-    # with open('output.json.json', 'r') as file:
-    #     df_dict = json.load(file)
 
     print(time.perf_counter() - x1)
 
